fb07bd6decacbfe512ca12b1859c7698f764bc0f
P1k4sh <p1k4sh@yandex.ru>
2022-11-29 16:09:34 +0300

    hw2 is done

--- src/Lesson2/Hw2/Homework2.java
+++ src/Lesson2/Hw2/Homework2.java
@@ -9,18 +9,16 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Homework2 {
     public static void main(String[] args) {
-        int[] arr1 = createArr(-10, 10, 10);
-        int[] arr2 = createArr(-10, 10, 10);
+        int[] arr1 = createArr(-10, 10, 15);
         System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.toString(mergeArrays(arr1, arr2)));
-
+        System.out.println(Arrays.toString(mergeArr(arr1)));
     }
 
     /**
      * Создаем массив из рандомных чисел
+     *
      * @param from начало границы рандома
-     * @param to конец границы рандома
+     * @param to   конец границы рандома
      * @param size размер массива
      * @return созданный массив int[] arr
      */
@@ -33,26 +31,91 @@ public class Homework2 {
         return arr;
     }
 
-    public static int[] mergeArrays(int[] a1, int[] a2) {
-        int[] b = new int[a1.length + a2.length];
-        int positionA1 = 0;
-        int positionA2 = 0;
+    /**
+     * Реализация ручного метода сортировки массива слиянием. Вкратце:
+     * На вход приходит массив, делим его на 2 равных(или примерно равных массива), оба массива сортируем
+     * по возрастанию, затем сравниваем поэлементно каждый элемент из массивов и наименьший добавляем в итоговый массив
+     * @param someArr - массив на вход, который надо отсортировать слиянием
+     * @return возвращаем отсортированный массив
+     */
+    public static int[] mergeArr(int[] someArr) {
+        int[] arr1 = divideArray(someArr, 1);
+        int arr1Index = 0;
+        sortArray(arr1);
+        int[] arr2 = divideArray(someArr, 2);
+        int arr2Index = 0;
+        sortArray(arr2);
+        int[] merged = new int[someArr.length];
 
-        for (int i = 0; i < b.length; i++) {
-            if (positionA1 == a1.length) {
-                b[i] = a2[positionA2];
-                positionA2++;
-            } else if (positionA2 == a2.length) {
-                b[i] = a1[positionA1];
-                positionA1++;
-            } else if (a1[positionA1] < a2[positionA2]) {
-                b[i] = a1[positionA1];
-                positionA1++;
+        for (int i = 0; i < someArr.length; i++) {
+            if (arr1Index == arr1.length) {
+                merged[i] = arr2[arr2Index];
+                arr2Index++;
+            } else if (arr2Index == arr2.length) {
+                merged[i] = arr1[arr1Index];
+                arr1Index++;
             } else {
-                b[i] = a2[positionA2];
-                positionA2++;
+                if (arr1[arr1Index] < arr2[arr2Index]) {
+                    merged[i] = arr1[arr1Index];
+                    arr1Index++;
+                } else {
+                    merged[i] = arr2[arr2Index];
+                    arr2Index++;
+                }
             }
         }
-        return b;
+        return merged;
+    }
+
+    /**
+     * Метод для деления массива на две равные(или примерно равные, в случае, если длина массива нечетная) части
+     * @param someArr - принимает на вход массив
+     * @param half - индекс половины (1я или 2я половина массива)
+     * @return возвращаем массив(необходимую половину массива)
+     */
+    public static int[] divideArray(int[] someArr, int half) {
+        int size = 0;
+        if (half == 1) {
+            size = someArr.length / 2;
+        } else {
+            size = someArr.length - someArr.length / 2;
+        }
+        int[] arr = new int[size];
+        if (half == 1) {
+            for (int i = 0; i < size; i++) {
+                arr[i] = someArr[i];
+            }
+        } else {
+            if (someArr.length % 2 == 0) {
+                for (int j = 0; j < size; j++) {
+                    arr[j] = someArr[size + j];
+                }
+            } else {
+                for (int j = 0; j < size; j++) {
+                    arr[j] = someArr[size - 1 + j];
+                }
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * Метод ручной сортировки массива по возрастанию
+     * @param someArr - принимаем массив на вход, который будем сортировать по возрастанию
+     * @return возвращаем отсортированный массив
+     */
+    public static int[] sortArray(int[] someArr) {
+        int temp = 0;
+        for (int i = 0; i < someArr.length - 1; i++) {
+            for (int j = 0; j < someArr.length; j++) {
+                if (someArr[j] > someArr[i + 1]) {
+                    temp = someArr[j];
+                    someArr[j] = someArr[i + 1];
+                    someArr[i + 1] = temp;
+                }
+            }
+        }
+        return someArr;
     }
 }
+
